5e0f2113a425d1a14a4aa01854045619b6db8d1a
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-06-03 07:29:14 +0530

    Fetching data with useReducer Part 2

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
 import './App.css';
 import React, { useReducer } from 'react';
-import ClassCounter from './components/ClassCounter';
-import Datafetching from './components/Datafetching';
-import HookCOunter from './components/HookCOunter';
-import HookCOunterFour from './components/HookCOunterFour';
-import HookCOunterTwo from './components/HookCOunterTwo';
-import HookCounterOne from './components/HookCounterOne';
-import HookCounterThree from './components/HookCounterThree';
-import HookMouse from './components/HookMouse';
-import IntervalHookCounter from './components/IntervalHookCounter';
-import MouseContainer from './components/MouseContainer';
-import CounterOne from './components/CounterOne';
-import CounterTwo from './components/CounterTwo';
-import CounterThree from './components/CounterThree';
 import DataFetchinOne from './components/DataFetchinOne';
+import DatafetchingTwo from './components/DatafetchingTwo';
 
 
 function App() {
@@ -21,10 +9,9 @@ function App() {
   return (
 
     <div className="App">
+      <DatafetchingTwo />
       {/* <Datafetching /> */}
-      <DataFetchinOne />
-
-
+      {/* <DataFetchinOne /> */}
       {/* <Datafetching /> */}
       {/* <IntervalHookCounter /> */}
       {/* <MouseContainer /> */}
