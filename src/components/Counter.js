33b852763fb27f664fdb6f75977c9458f56288d0
abhisheksavaliya555 <abhisheksavaliya555@gmail.com>
2023-06-03 08:12:00 +0530

    useMemo Hook

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useMemo } from 'react'
 
 function Counter() {
     const [counterOne, setCounterOne] = useState(0)
@@ -12,17 +12,17 @@ function Counter() {
         setCounterTwO(counterTwo + 1)
     }
 
-    const isEven = () => {
+    const isEven = useMemo(() => {
         let i = 0
-        while(i<2000000000) i++
+        while (i < 2000000000) i++
         return counterOne % 2 === 0
-    }
+    }, [counterOne])
 
 
     return (
         <div>
             <button onClick={incrementOne}>Count One - {counterOne}</button>
-            <span>{isEven() ? 'Even' : 'Odd'}</span>
+            <span>{isEven ? 'Even' : 'Odd'}</span>
             <div>
                 <button onClick={incrementTwO}>Count One - {counterTwo}</button>
             </div>
